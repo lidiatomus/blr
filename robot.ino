b8e753a396159e70630e17ac20dd43abde5e8ddf
lidiatomus <158284726+lidiatomus@users.noreply.github.com>
2024-05-07 20:14:58 +0300

    Add files via upload

--- robot.ino
+++ robot.ino
@@ -2,26 +2,26 @@
 #include <QTRSensors.h>
 
 // definire pini
-#define LINE_SENSOR1_PIN A0
-#define LINE_SENSOR2_PIN A1
-#define OPPO_SENSOR_1_PIN 2
-#define OPPO_SENSOR_2_PIN 3
-#define OPPO_SENSOR_3_PIN 4
-#define OPPO_SENSOR_4_PIN 5
-#define OPPO_SENSOR_5_PIN 6
-#define START_STOP_MODULE_PIN 7
-#define MOTOR_DRIVER_1_PWM_PIN 9
-#define MOTOR_DRIVER_2_PWM_PIN 10
-#define MOTOR_DRIVER_1_DIR_PIN 8
-#define MOTOR_DRIVER_2_DIR_PIN 11
+#define LINE_SENSOR1_PIN 2
+#define LINE_SENSOR2_PIN 3
+#define OPPO_SENSOR_1_PIN 4
+#define OPPO_SENSOR_2_PIN 5
+#define OPPO_SENSOR_3_PIN 6
+#define OPPO_SENSOR_4_PIN 7
+#define OPPO_SENSOR_5_PIN 8
+#define START_STOP_MODULE_PIN 9
+#define MOTOR_DRIVER_1_PWM_PIN 10
+#define MOTOR_DRIVER_2_PWM_PIN 11
+#define MOTOR_DRIVER_1_DIR_PIN 12
+#define MOTOR_DRIVER_2_DIR_PIN 13
 
 // definire constante
 #define NUM_OPPO_SENSORS 5
 #define NUM_LINE_SENSORS 2
 #define NUM_SENSORS_PER_OPPO 1
 
-// creare array pt senzorii de linie
-QTRSensorsAnalog lineSensors((unsigned char[]) {LINE_SENSOR1_PIN, LINE_SENSOR2_PIN}, NUM_LINE_SENSORS);
+// Creaare obiect pt senzorii de oponent
+QTRSensors oppoSensors;
 
 bool isRobotRunning = false;
 
@@ -47,16 +47,11 @@ void loop() {
 }
 
 void initializeSensors() {
-  // senzori de linie
-  lineSensors.init();
-  lineSensors.setTimeout(2000); // Set timeout to 2000ms, asa a sugerat chat gpt ul ca sa se initializeze senzorii corect
+  // Initializare senzor de linie
+  unsigned char lineSensorPins[NUM_LINE_SENSORS] = {LINE_SENSOR1_PIN, LINE_SENSOR2_PIN}; 
+  // Initializare senzor de oponent
+  unsigned char opponentSensorPins[NUM_OPPO_SENSORS] = {OPPO_SENSOR_1_PIN, OPPO_SENSOR_2_PIN, OPPO_SENSOR_3_PIN, OPPO_SENSOR_4_PIN, OPPO_SENSOR_5_PIN};
 
-  // senzorii de oponent
-  pinMode(OPPO_SENSOR_1_PIN, INPUT);
-  pinMode(OPPO_SENSOR_2_PIN, INPUT);
-  pinMode(OPPO_SENSOR_3_PIN, INPUT);
-  pinMode(OPPO_SENSOR_4_PIN, INPUT);
-  pinMode(OPPO_SENSOR_5_PIN, INPUT);
 }
 
 void initializeMotors() {
@@ -74,7 +69,7 @@ void initializeStartStopModule() {
 bool wasModulePressed = false; // ca sa verificam cand e apasat din nou start stop module (la prima apasare porneste robotul la a doua se opreste)
 
 void checkStartStopModule() {
-  int module = digitalRead(START_STOP_MODULE_PIN);
+  int moduleState = digitalRead(START_STOP_MODULE_PIN);
   
   // daca primim semnal de la module pt prima oara robotul porneste
   if (moduleState == HIGH && !wasModulePressed) {
@@ -88,7 +83,6 @@ void checkStartStopModule() {
     isRobotRunning = false;
     wasModulePressed = false;
   }
-}
 }
 
 void startRobot() {
