c4cb749df9725f14f8fd452449b3ba405f47a177
lidiatomus <158284726+lidiatomus@users.noreply.github.com>
2024-05-18 21:15:56 +0300

    Add files via upload

--- robot.ino
+++ robot.ino
@@ -9,12 +9,13 @@
 #define OPPO_SENSOR_3_PIN 6
 #define OPPO_SENSOR_4_PIN 7
 #define OPPO_SENSOR_5_PIN 8
-#define START_STOP_MODULE_PIN 9
+#define START_STOP_MODULE_START_PIN 9
 #define MOTOR_DRIVER_1_PWM_PIN 10
 #define MOTOR_DRIVER_2_PWM_PIN 11
 #define MOTOR_DRIVER_1_DIR_PIN 12
 #define MOTOR_DRIVER_2_DIR_PIN 13
 #define LINE_THRESHOLD 500
+
 // definire constante
 #define NUM_OPPO_SENSORS 5
 #define NUM_LINE_SENSORS 2
@@ -53,12 +54,12 @@ void loop() {
 
   // Make decisions based on sensor readings
   if (isRobotRunning) {
-    if (isOpponentDetected) {
-      runAwayFromOpponent();
-    } else if (isLineDetected) {
+    if (isOpponentDetected && isLineDetected) {
+      attack_Opponent();
+    } else if (isLineDetected && !isOpponentDetected) {
       followWhiteLine();
-    } else {
-      stopRobot();
+    } else if(!isLineDetected && isOpponentDetected) {
+      runAwayFromOpponent();
     }
   }
 }
@@ -80,16 +81,16 @@ void initializeMotors() {
 }
 
 void initializeStartStopModule() {
-  pinMode(START_STOP_MODULE_PIN, INPUT);
+  pinMode(START_STOP_MODULE_START_PIN, INPUT);
 }
 
 bool wasModulePressed = false; // ca sa verificam cand e apasat din nou start stop module (la prima apasare porneste robotul la a doua se opreste)
 
 void checkStartStopModule() {
-  int moduleState = digitalRead(START_STOP_MODULE_PIN);
+  int moduleState = digitalRead(START_STOP_MODULE_START_PIN);
   
   // daca primim semnal de la module pt prima oara robotul porneste
-  if (moduleState == HIGH && !wasModulePressed) {
+  if (moduleState == LOW && !wasModulePressed) {
     startRobot();
     isRobotRunning = true;
     wasModulePressed = true;
@@ -100,14 +101,15 @@ void checkStartStopModule() {
     isRobotRunning = false;
     wasModulePressed = false;
   }
+  
 }
 
 void startRobot() {
   digitalWrite(MOTOR_DRIVER_1_DIR_PIN, HIGH); 
   digitalWrite(MOTOR_DRIVER_2_DIR_PIN, HIGH);
   //directii
-  analogWrite(MOTOR_DRIVER_1_PWM_PIN, 255); // viteza trebuie gandita pt strategie, acum e maxima
-  analogWrite(MOTOR_DRIVER_2_PWM_PIN, 255); // 
+  analogWrite(MOTOR_DRIVER_1_PWM_PIN, 100); // viteza trebuie gandita pt strategie, acum e maxima
+  analogWrite(MOTOR_DRIVER_2_PWM_PIN, 100); // 
   Serial.println("Robot started"); // for debugging
 }
 
@@ -141,30 +143,73 @@ void updateSensorReadings() {
 void runAwayFromOpponent() {
   // Setam motoare sa fugim de oponent
   // daca e la stanga mergem la dreapta, si invers pt stanga
-  if (lastOpponentPosition < (NUM_OPPO_SENSORS / 2)) {
-    digitalWrite(MOTOR_DRIVER_1_DIR_PIN, HIGH);
-    digitalWrite(MOTOR_DRIVER_2_DIR_PIN, HIGH);
+  if (lastOpponentPosition == 0) {
+    // Opponent detected on the left
+    digitalWrite(MOTOR_DRIVER_1_DIR_PIN, HIGH);  // turn right
+    digitalWrite(MOTOR_DRIVER_2_DIR_PIN, LOW);   // move fwd
+  } else if (lastOpponentPosition == 1 || lastOpponentPosition == 2) {
+    // Opponent detected at the front (center or right)
+    digitalWrite(MOTOR_DRIVER_1_DIR_PIN, LOW);   // Move backward
+    digitalWrite(MOTOR_DRIVER_2_DIR_PIN, LOW);   // Move backward
+  } else if (lastOpponentPosition == 3 || lastOpponentPosition == 2) {
+    // Opponent detected on the left side
+    digitalWrite(MOTOR_DRIVER_1_DIR_PIN, HIGH);  // Move forward
+    digitalWrite(MOTOR_DRIVER_2_DIR_PIN, LOW);   // Turn right
+  } else if (lastOpponentPosition == 4) {
+    // Opponent detected on the right side
+    digitalWrite(MOTOR_DRIVER_1_DIR_PIN, LOW);   // Turn left
+    digitalWrite(MOTOR_DRIVER_2_DIR_PIN, HIGH);  // Move forward
   } else {
-    digitalWrite(MOTOR_DRIVER_1_DIR_PIN, LOW);
-    digitalWrite(MOTOR_DRIVER_2_DIR_PIN, LOW);
+    digitalWrite(MOTOR_DRIVER_1_DIR_PIN, LOW);   // Turn left
+    digitalWrite(MOTOR_DRIVER_2_DIR_PIN, HIGH);  // Move forward
   }
-  analogWrite(MOTOR_DRIVER_1_PWM_PIN, 200);
-  analogWrite(MOTOR_DRIVER_2_PWM_PIN, 200);
+  analogWrite(MOTOR_DRIVER_1_PWM_PIN, 100);
+  analogWrite(MOTOR_DRIVER_2_PWM_PIN, 100);
+  delay(3000); // Wait for 3 seconds (10000 milliseconds)
+  digitalWrite(MOTOR_DRIVER_1_DIR_PIN, HIGH);  // Move forward
+  digitalWrite(MOTOR_DRIVER_2_DIR_PIN, LOW);   // Turn right
+  updateSensorReadings(); // Update sensor readings
 }
-
+void attack_Opponent() {
+  // Setam motoare sa fugim de oponent
+  // daca e la stanga mergem la dreapta, si invers pt stanga
+  if (lastOpponentPosition == 0) {
+    // Opponent detected on the left
+    digitalWrite(MOTOR_DRIVER_1_DIR_PIN, LOW);  // Move forward
+    digitalWrite(MOTOR_DRIVER_2_DIR_PIN, HIGH);   // Turn LEFT
+  } else if (lastOpponentPosition == 1 || lastOpponentPosition == 2) {
+    // Opponent detected at the front (center or right)
+    digitalWrite(MOTOR_DRIVER_1_DIR_PIN, HIGH);   // Move backward
+    digitalWrite(MOTOR_DRIVER_2_DIR_PIN, HIGH);   // Move backward
+  } else if (lastOpponentPosition == 3) {
+    // Opponent detected on the RIGHT side
+    digitalWrite(MOTOR_DRIVER_1_DIR_PIN,LOW);  // Move forward
+    digitalWrite(MOTOR_DRIVER_2_DIR_PIN, HIGH);   // Turn right
+  } else if (lastOpponentPosition == 4) {
+    // Opponent detected on the right side
+    digitalWrite(MOTOR_DRIVER_1_DIR_PIN, HIGH);   // Turn RIGH
+    digitalWrite(MOTOR_DRIVER_2_DIR_PIN, LOW);  // Move FOWARD
+  }
+  analogWrite(MOTOR_DRIVER_1_PWM_PIN, 150);
+  analogWrite(MOTOR_DRIVER_2_PWM_PIN, 150);
+  updateSensorReadings(); // Update sensor readings
+}
 void followWhiteLine() {
   // modificam mersul ca sa ramanem in ring
-  //daca stanga atunci dreapta si invers, daca nimic atunci mergem in fata
-  if (lineSensorValues[0] < LINE_THRESHOLD) {
-    digitalWrite(MOTOR_DRIVER_1_DIR_PIN, LOW);
-    digitalWrite(MOTOR_DRIVER_2_DIR_PIN, HIGH);
-  } else if (lineSensorValues[1] < LINE_THRESHOLD) {
+  //daca FATA -> VAL 0 , ATUNCI DAM CU SPATELE 
+  if (lineSensorValues[0] > LINE_THRESHOLD) { // cat timp vede pe fata alba
+    while (lineSensorValues[1] < LINE_THRESHOLD) { //si pe spate negru 180 
+      digitalWrite(MOTOR_DRIVER_1_DIR_PIN, LOW);  // Move backward
+      digitalWrite(MOTOR_DRIVER_2_DIR_PIN, HIGH); // Move backward
+    }
+  } else if (lineSensorValues[1] > LINE_THRESHOLD) {
     digitalWrite(MOTOR_DRIVER_1_DIR_PIN, HIGH);
-    digitalWrite(MOTOR_DRIVER_2_DIR_PIN, LOW);
+    digitalWrite(MOTOR_DRIVER_2_DIR_PIN, HIGH);
   } else {
     digitalWrite(MOTOR_DRIVER_1_DIR_PIN, HIGH);
     digitalWrite(MOTOR_DRIVER_2_DIR_PIN, HIGH);
   }
-  analogWrite(MOTOR_DRIVER_1_PWM_PIN, 200);
-  analogWrite(MOTOR_DRIVER_2_PWM_PIN, 200);
+  analogWrite(MOTOR_DRIVER_1_PWM_PIN, 130);
+  analogWrite(MOTOR_DRIVER_2_PWM_PIN, 130);
+  updateSensorReadings(); // Update sensor readings
 }
